b52e3fad195518f381134e94f6252ee21fd59881
Andriana Oridoroha <oridorohaa@gmail.com>
2022-11-25 07:11:31 -0600

    finish without optionals

---
+++ app/models/movie.rb
@@ -0,0 +1,12 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  # has_many :lists, through: :bookmarks
+  
+  # You can’t delete a movie if it is referenced in at least one bookmark.
+  # this by defaut will not allow you to delete a movie
+  # if it is connected to bookmarks
+  # if you try to delete it it will say you have bookmark connections
+  # and it cannot delete
+  validates :title, uniqueness: true
+  validates :overview, uniqueness: true
+end
---
+++ db/migrate/20221124205914_add_to_list_imageurl.rb
@@ -0,0 +1,6 @@
+class AddToListImageurl < ActiveRecord::Migration[7.0]
+  def change
+    add_column :lists, :image_url, :string
+
+  end
+end
