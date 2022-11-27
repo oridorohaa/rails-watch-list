7a17a1489a9655600dd60bbbd63fc7d20be1fed3
Andriana Oridoroha <oridorohaa@gmail.com>
2022-11-27 09:21:41 -0600

    cloudirany done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,7 +18,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -28,7 +28,7 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :image_url)
+    params.require(:list).permit(:name, :photo)
   end
 
   def set_bookmark
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,16 +21,16 @@ results.each do |result|
   Movie.create!(title: result["title"], overview: result["overview"], poster_url: result["poster_path"], rating: result["vote_average"])
 end
 
-List.destroy_all
-List.create(name: "Classsic Movies", image_url:"https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60")
-List.create(name: "Foreign Films", image_url:"https://images.unsplash.com/photo-1573455494060-c5595004fb6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8amFwYW5lc2UlMjBzdHJlZXRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60")
-List.create(name:"Comedy", image_url:"https://images.unsplash.com/photo-1543584756-8f40a802e14f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80")
+# List.destroy_all
+# List.create(name: "Classsic Movies", image_url:"https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60")
+# List.create(name: "Foreign Films", image_url:"https://images.unsplash.com/photo-1573455494060-c5595004fb6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8amFwYW5lc2UlMjBzdHJlZXRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60")
+# List.create(name:"Comedy", image_url:"https://images.unsplash.com/photo-1543584756-8f40a802e14f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80")
 
-Bookmark.destroy_all
-book_one = Bookmark.new(comment: "Amazing classic, a must watch")
-book_one.movie = Movie.find_by(id:100)
-book_one.list = List.first
-book_one.save
+# Bookmark.destroy_all
+# book_one = Bookmark.new(comment: "Amazing classic, a must watch")
+# book_one.movie = Movie.find_by(id:100)
+# book_one.list = List.first
+# book_one.save
 # Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 # Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 # Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
